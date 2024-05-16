af522b4c2bc389887eed1c26b33fd530148c017a
vivek-roots <natvivek@gmail.com>
2024-05-16 21:43:42 +0530

    About us page content removed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,44 +7,8 @@ function About() {
       <Header />
       <div className="h-full mt-20">
         <div className="flex flex-col px-5 space-y-5 lg:px-72">
-          <h2 className="text-center">ABOUT ROSCH (from IDS)</h2>
-
-          <div className="flex flex-col space-y-5">
-            <p>
-              Our company, ROSCH, is a company deeply dedicated to ensuring the
-              highest level of customer satisfaction. We take pride in our
-              position as a global leader in the manufacturing of cutting-edge
-              medical devices designed for a wide range of applications. Our
-              expertise and specialization extend to the production of versatile
-              laser devices that find applications in the fields of dermatology,
-              aesthetics, surgery, dentistry, cosmetics, and veterinary care.
-            </p>
-            <p>
-              Over the years, our unwavering commitment to innovation has
-              allowed us to tailor our technology to meet the unique needs of
-              our customers. As a result, we have expanded our reach, exporting
-              our high-quality dermatologic, aesthetic, surgical, dental,
-              cosmetic, and veterinary devices to more than 45 countries
-              worldwide.
-            </p>
-            <p>
-              Our driving enthusiasm is encapsulated in our company slogan:
-              "Innovation for Better Life." We are dedicated to pushing the
-              boundaries of what is possible, continually striving to improve
-              the lives of individuals and communities around the world through
-              our advanced medical device and solutions.
-            </p>
-            <p>
-              We look forward to growing alongside our valued global partners
-              and customers, making a positive impact through innovation, and
-              contributing to a better quality of life for all.
-            </p>
-          </div>
-        </div>
-
-        <div className="flex flex-col px-5 space-y-5 lg:px-72">
           <h2 className="text-center">ABOUT US</h2>
-    
+
           <div className="flex flex-col space-y-5">
             <p>
               ROSCH MEDICAL SYSTEM (P) Ltd is pioneer manufacturing medical and
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,10 +9,16 @@ function Home() {
         <Header />
         <div className="h-full">
           <div className="z-30 flex h-screen mt-12 lg:h-auto lg:ml-32">
-            <h2 className="flex flex-col justify-center ml-10 lg:mt-40 lg:ml-40">
-              <span className="font-semibold text-white"> Revolutionize </span>
-              <span className="font-semibold text-white"> Enchant </span>
-              <span className="font-semibold text-white"> Gratification </span>
+            <h2 className="flex flex-col justify-center ml-10 space-y-5 lg:mt-40 lg:ml-40">
+              <span className="text-6xl font-semibold tracking-wider text-white">
+                Revolutionize
+              </span>
+              <span className="text-6xl font-semibold tracking-wider text-white">
+                Enchant
+              </span>
+              <span className="text-6xl font-semibold tracking-wider text-white">
+                Gratification
+              </span>
             </h2>
           </div>
           <img
