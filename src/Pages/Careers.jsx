6694f171a37dddc82ce3084918a146247ae5bdfb
vivek-roots <natvivek@gmail.com>
2024-06-18 12:22:01 +0530

    Addin name attributes to html forms

--- src/Pages/Careers.jsx
+++ src/Pages/Careers.jsx
@@ -8,18 +8,19 @@ export default function Careers() {
     <Layout>
       <div className="md:mt-20 md:h-screen">
         <div className="absolute top-0 h-full -z-10">
-            <img
-              src={contactUsSvg}
-              alt=""
-              className="object-cover h-screen lg:h-auto md:w-full"
-            />
-          </div>
+          <img
+            src={contactUsSvg}
+            alt=""
+            className="object-cover h-screen lg:h-auto md:w-full"
+          />
+        </div>
         <div className="flex flex-col space-y-5 pt-32 text-primarypr-10 pl-10 lg:pl-[55%]">
           <form name="careers" method="post" className="flex flex-col pr-10">
             <h2 className="mb-4 font-bold uppercase">Join Us</h2>
             <input type="hidden" name="form-name" value="careers" />
 
             <input
+              name="name"
               required
               type="text"
               placeholder="Name*"
@@ -28,29 +29,25 @@ export default function Careers() {
             <input
               required
               type="email"
-              name=""
-              id=""
+              name="emil"
               placeholder="Email*"
               className="input-text"
             />
             <input
               required
               type="tel"
-              name=""
-              id=""
+              name="phone"
               placeholder="Phone number*"
               className="input-text"
             />
             <input
               type="file"
-              name=""
-              id=""
+              name="resume"
               placeholder="Resume"
               className="input-text"
             />
             <textarea
-              name=""
-              id=""
+              name="description"
               rows="10"
               placeholder="Description"
               className="text-area"
