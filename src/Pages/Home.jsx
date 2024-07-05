fdaf7973dcced8395cee534c21542abb59dad9dc
vivek-roots <natvivek@gmail.com>
2024-07-05 10:26:01 +0530

    rollup issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import Layout from "@/Components/Layout";
 
 import picoPremium from "@/assets/pico/pico-premium.png";
 import sonopoint from "@/assets/sonopoint/sonopoint.png";
+import "./carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
 function Home() {
