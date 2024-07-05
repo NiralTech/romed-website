5cf990c8bfdfc55740890dc71b3e67f87ee9e012
vivek-roots <natvivek@gmail.com>
2024-07-05 10:17:41 +0530

    carousel css issue fixed.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Layout from "@/Components/Layout";
 
 import picoPremium from "@/assets/pico/pico-premium.png";
 import sonopoint from "@/assets/sonopoint/sonopoint.png";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "@/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
 function Home() {
